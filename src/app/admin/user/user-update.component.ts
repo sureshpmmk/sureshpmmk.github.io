428fc9a6ea3148d410d475db0540e1b5223b679f
Lakshmikanth V Mallar <lvm@premiergp.com>
2017-04-18 13:06:30 +0530

    Completed Update Project and Update User.
    Signed-off-by: Lakshmikanth V Mallar <lvm@premiergp.com>

---
+++ src/app/admin/user/user-update.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { User } from '../../shared/user.model';
+import { UserService } from '../../shared/user.service';
+
+@Component({
+  templateUrl: './user-update.component.html'
+})
+export class UserUpdateComponent implements OnInit, OnDestroy {
+  @ViewChild('f') userForm: NgForm;
+  users;
+  user;
+  passedEmployeeid
+  employeeid;
+  firstname;
+  lastname;
+  email;
+  phonenumber;
+  password;
+  usertype;
+  shift;
+  status;
+  userSubscription;
+  userIndex;
+
+  constructor(private userService: UserService,
+              private route: ActivatedRoute,
+              private router: Router) {
+    //this.startdate = new Date().toISOString().slice(0, 16);
+  }
+
+  ngOnInit() {
+    this.userSubscription = this.route.params.subscribe(params => {
+      this.passedEmployeeid = params['employeeId']; 
+    });
+
+    this.getOneUser(this.passedEmployeeid);    
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
+
+  getOneUser(passedEmployeeid: string) {
+    this.user = this.userService.getOneUser(passedEmployeeid);
+
+    this.employeeid  = this.user.employeeid;
+    this.firstname   = this.user.firstname;
+    this.lastname    = this.user.lastname;
+    this.email       = this.user.email;
+    this.phonenumber = this.user.phonenumber;
+    this.password    = this.user.password;
+    this.usertype    = this.user.usertype;
+    this.shift       = this.user.shift;
+    this.status      = this.user.status;
+  }
+
+  updateUser() {
+    this.users = this.userService.getUsers();
+    this.userService.usersChanged
+        .subscribe(
+          (users: User[]) => {
+            this.users = users;
+        });
+  	
+    this.employeeid = this.userForm.value['employeeid'];
+    this.userIndex = this.users.map((user) => user.employeeid).indexOf(this.employeeid); 
+  	this.userService.updateUserDetails(this.userIndex, this.userForm.value);
+    this.router.navigate(['/admin/manage-user']);
+  }
+
+  cancel() {
+    this.router.navigate(['/admin/manage-user']);
+  }
+
+}
