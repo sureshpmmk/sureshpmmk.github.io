1400dbb59266f1fd6a6623c7be587f0c4a6ae23d
Lakshmikanth V Mallar <lvm@premiergp.com>
2017-04-20 13:00:33 +0530

    Updated log service.
    Signed-off-by: Lakshmikanth V Mallar <lvm@premiergp.com>

--- src/app/shared/logs.service.ts
+++ src/app/shared/logs.service.ts
@@ -1,9 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 import { Logs } from './logs.model';
 
+@Injectable()
 export class LogsService { 
-  private log: Logs[] = require('./logs.json');
+  logsChanged = new Subject<Logs[]>();
+  private logs: Logs[] = []; //require('./logs.json'); //
 
-  getRecentLogs(employeeid : string) {  
-    return this.log.find(e => e.employeeid === employeeid); 
+  constructor(private http: Http) {
+    this.getLogsJson();
+  }
+
+  getLogsJson() {
+    this.http.get("./src/app/shared/logs.json")
+             .map(
+                (response: Response) => {
+                  const logs: Logs[] = response.json();
+                  for(let log of logs) {
+                    if(!log) {
+                      console.log(log);
+                    }
+                  }
+                  return logs;
+                }
+              )
+             .subscribe(
+               (logs: Logs[]) => {                 
+                 this.setLogs(logs);
+               }
+              );
+  }
+
+  setLogs(logs: Logs[]) {
+    this.logs = logs;
+    this.logsChanged.next(this.logs.slice());
+  }
+
+  getAllLogs() {  
+    return this.logs.slice(); 
+  }
+
+  getRecentLogs(employeeid : string) {
+    return this.logs.find(e => e.employeeid === employeeid); 
   }
 }
