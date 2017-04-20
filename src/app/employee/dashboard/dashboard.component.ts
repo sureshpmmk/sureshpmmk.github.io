67a5bb8647374d472d094328756dd650f327b546
Subina K K <skk@premiergp.com>
2017-04-20 17:37:16 +0530

    STTA-19 Frontend - Dasbhoard - Recent Logs
    Signed-off-by: Subina K K <skk@premiergp.com>

--- src/app/employee/dashboard/dashboard.component.ts
+++ src/app/employee/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 import { Logs } from '../../shared/logs.model';
 import { LogsService } from '../../shared/logs.service'; 
@@ -9,14 +10,16 @@ import { LogsService } from '../../shared/logs.service';
 })
 export class DashboardComponent implements OnInit {
  //logs: Logs[] = [];
-  private logs;
+  date;
+  public logsArray= [];
   constructor(private logsService: LogsService, private router: Router){}
   public loggedUser = localStorage.getItem("username");
   public user = JSON.parse(localStorage.getItem("user"));
   
   ngOnInit() {
-    this.logs = this.logsService.getRecentLogs(this.user.employeeid);  
-    console.log(this.logs);
+    this.date = new Date();
+    //this.logsArray = this.logsService.getRecentLogs(this.user.employeeid);  
+   
     if(this.loggedUser == null)
     {
     	 this.router.navigate(['pages/login']);
