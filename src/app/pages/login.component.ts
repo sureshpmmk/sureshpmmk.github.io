4e0b395ea876bbf3351cd46080a181e38f8e94e7
Mahesh M <mmadhavan@premiergp.com>
2017-05-05 14:58:49 +0530

    STAT-87-Completed
    Signed-off-by: Mahesh M <mmadhavan@premiergp.com>

--- src/app/pages/login.component.ts
+++ src/app/pages/login.component.ts
@@ -10,18 +10,46 @@ import { AuthenticationService } from '../shared/authentication.service';
 export class LoginComponent {
 
   flag;
-  success = true;
-  validationError = '';
+  success;
+  validationError;
+  validationError1;
+  validationError2;
+  auth;
   //Getting form value and assigning to loginForm	
   @ViewChild('f') loginForm: NgForm; 	
   constructor(private authService : AuthenticationService) { }
 
   //Validating User
   onLogin(){ 
-	this.flag = this.authService.loginUser(this.loginForm.value);
+
+  this.validationError='';
+  this.validationError1 = '';
+  this.validationError2 = '';
+  this.flag = true;
+  this.success = true;  
+  if((this.loginForm.value.username === "") && (this.loginForm.value.password === "")){
+    this.success = false;
+    this.validationError = "Please fill both fields";
+    this.flag === false;
+
+    return;
+  }
+  if(this.loginForm.value.username == ""){
+    this.validationError1 = "User name cannot be blank.";
+    this.flag === false;
+    return;
+  }
+  if(this.loginForm.value.password == ""){
+    this.validationError2 = "Password cannot be blank.";
+    this.flag === false;
+    return;
+  }    
+	if(this.flag === true){    
+    this.flag = this.authService.loginUser(this.loginForm.value);
+  }
 	if(this.flag === false)
 	{
-		this.validationError = 'Authentication failed.';
+		this.validationError = 'Invalid login or password.';
 		this.loginForm.reset();
     this.success = false;
 	}
